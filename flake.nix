0729d00e7b170ff44d84787a168dbd41570f199b
vvzvlad <github@vvzvlad.xyz>
2022-05-30 16:36:22 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/22.05-pre";
   inputs.home-manager.url = "gitlab:rycee/home-manager/release-21.11";
   outputs = { self, nixpkgs, home-manager }@inputs: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.nixos-rdp-server = nixpkgs.lib.nixosSystem {
       modules = [ ./configuration.nix ];
       specialArgs.inputs = inputs;
       system = "x86_64-linux";
